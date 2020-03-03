a5e16d73c2990fd0027842f04b24cfc270f3caf0
ozan <goldenhawkk@gmail.com>
2020-03-03 16:29:40 +0300

    User.Identity.Name ifadesinin üstesinden gelmek

--- Blog.Tests/HomeControllerTests.cs
+++ Blog.Tests/HomeControllerTests.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using Blog.Controllers;
+using Blog.Controllers.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
@@ -18,6 +22,31 @@ namespace Blog.Tests
 
             // ASSERT
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void PrivacyShouldReturnPrivacyViewModelWithCorrectLoggedInUsername()
+        {
+            // ARRANGE
+            HomeController homeController = new HomeController();
+            homeController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>()
+                    {
+                        new Claim(ClaimTypes.Name, "test_user")
+                    }))
+                }
+            };
+
+            // ACT
+            IActionResult result = homeController.Privacy();
+
+            // ASSERT
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var privacyViewModel = Assert.IsType<PrivacyViewModel>(viewResult.Model);
+            Assert.Equal("test_user", privacyViewModel.Username);
         }
     }
 }
