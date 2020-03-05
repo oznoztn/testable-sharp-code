4524176dbbb7f64794fdfb481937f89ed4be44e8
ozan <goldenhawkk@gmail.com>
2020-03-05 09:53:12 +0300

    pictureUrl'in null olma durumunun test edilmesi

---
+++ Blog.Tests/UserControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Blog.Tests
+{
+    public class UserControllerTests
+    {
+        [Fact]
+        public async Task ChangeProfilePictureShouldReturnBadRequestWhenPictureUrlIsNull()
+        {
+            // arrange
+            UsersController usersController = new UsersController();
+
+            // act
+            var result = await usersController.ChangeProfilePicture(null);
+
+            // assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}
