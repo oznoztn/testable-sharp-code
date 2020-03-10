7768b7cde6cb35b9cb2553e35b92a389c92c2de6
ozan <goldenhawkk@gmail.com>
2020-03-10 17:06:26 +0300

    IDateTimeService için fake implementasyon

---
+++ Blog.Tests/Services/FakeDateTimeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blog.Services;
+
+namespace Blog.Tests.Services
+{
+    public class FakeDateTimeService : IDateTimeService
+    {
+        public DateTime DateTime { get; set; }
+        public DateTime Now()
+        {
+            return DateTime;
+        }
+
+        public DateTime UtcNow()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
