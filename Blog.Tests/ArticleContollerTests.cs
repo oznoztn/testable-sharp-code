6e01170b28a3a04bc78a28e534936cb3c5d9b9d8
ozan <goldenhawkk@gmail.com>
2020-03-11 15:02:35 +0300

    Random metodunun test edilmesi

--- Blog.Tests/ArticleContollerTests.cs
+++ Blog.Tests/ArticleContollerTests.cs
@@ -8,7 +8,9 @@ using Blog.Controllers;
 using Blog.Controllers.Models;
 using Blog.Services;
 using Blog.Services.Infrastructure;
+using Blog.Services.Models;
 using Blog.Tests.Data;
+using Blog.Tests.Extensions;
 using Blog.Tests.Fakes;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
@@ -22,6 +24,7 @@ namespace Blog.Tests
         [Fact]
         public async Task AllShouldReturnModelWithCorrectInformation()
         {
+            // ARRANGE
             const string database = "AllShouldReturnModelWithCorrectInformation";
             const int pageSize = 5;
             const int totalArticlesCount = 25;
@@ -43,11 +46,48 @@ namespace Blog.Tests
             var articleService = new ArticleService(context, mapper, dataTimeService);
             var controller = new ArticlesController(articleService, mapper, configuration);
 
+            
+            // ACT
             ViewResult result = Assert.IsType<ViewResult>(await controller.All());
             ArticleListingViewModel model = Assert.IsType<ArticleListingViewModel>(result.Model);
 
+            // ASSERT
             Assert.Equal(pageSize, model.Articles.Count());
             Assert.Equal(totalArticlesCount, model.Total);
+        }
+
+        [Fact]
+        public async Task RandomShouldReturnViewModelWithCorrecArticle()
+        {
+            // ARRANGE
+            const int requestedEntityIndex = 3;
+            const int requestedEntityId = requestedEntityIndex + 1;
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new KeyValuePair<string, string>[]
+                {
+                    new KeyValuePair<string, string>("Articles:PageSize", "5"),
+                })
+                .Build();
+
+            var articleController = 
+                new ArticlesController(new FakeArticleService(), null, configuration)
+                    .WithClaimPrincipal("TEST_USERNAME");
+
+            var fakeRandomValueProvider = new FakeRandomValueProvider
+            {
+                NextValue = requestedEntityIndex
+            };
+
+            // ACT
+            var actionResult =
+                Assert.IsType<ViewResult>(await articleController.Random(fakeRandomValueProvider));
+
+            var model = Assert.IsType<ArticleDetailsServiceModel>(actionResult.Model);
+
+            // ASSERT
+
+            Assert.Equal(requestedEntityId, model.Id);
         }
     }
 }
