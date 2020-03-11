853064ce2ee1a516b5ba76b5930b8065019d983c
ozan <goldenhawkk@gmail.com>
2020-03-11 14:59:22 +0300

    Add FakeRandomValueProvider

---
+++ Blog.Tests/Fakes/FakeRandomValueProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blog.Services;
+
+namespace Blog.Tests.Fakes
+{
+    public class FakeRandomValueProvider : IRandomValueProvider
+    {
+        public int NextValue { get; set; }
+        public int Next(int min, int max) => NextValue;
+    }
+}
