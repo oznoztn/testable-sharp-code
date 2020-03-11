16f956faeb5a7d073f0f9187390014d1bc7d0580
ozan <goldenhawkk@gmail.com>
2020-03-11 14:58:18 +0300

    Add RandomValueProvider to inject into ArticlesController

---
+++ Blog.Services/IRandomValueProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Services
+{
+    public interface IRandomValueProvider
+    {
+        int Next(int min, int max);
+    }
+}
---
+++ Blog.Services/RandomValueProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Services
+{
+    public class RandomValueProvider : IRandomValueProvider
+    {
+        private static Random _random;
+
+        public RandomValueProvider(int seed)
+        {
+            _random = new Random(seed);   
+        }
+
+        public int Next(int min, int max) => _random.Next(min, max);
+    }
+}
